dc174098e11c3952c4d2c2a643adae941bad08d5
Safwan Ahmad <innoxentsayyed@gmail.com>
2025-06-25 14:40:50 +0530

    add solution 4

---
+++ Problem-4.js
@@ -0,0 +1,31 @@
+/*
+
+ Problem-4: Get the total count of number listed in the dictionary which is multiple of [1,2,3,4,5,6,7,8,9]
+  (examples)
+  input: [1,2,8,9,12,46,76,82,15,20,30]
+  Output: 
+    {1: 11, 2: 8, 3: 4, 4: 4, 5: 3, 6: 2, 7: 0, 8: 1, 9: 1}
+
+    */
+
+    function countMultiples(arr) {
+    const result = {};
+
+    for (let i = 1; i <= 9; i++) {
+        result[i] = 0;
+    }
+
+    for (const num of arr) {
+        for (let i = 1; i <= 9; i++) {
+            if (num % i === 0) {
+                result[i]++;
+            }
+        }
+    }
+
+    console.log(result);
+}
+
+// Example usage:
+const input = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30];
+countMultiples(input);
